2e87cd61b487116dce74018dddf17eb2343a7e2c
chtkg <152046353+chtkg@users.noreply.github.com>
2026-05-10 18:50:42 +0800

    Delete model directory

--- model/ST_Encoder/__init__.py
+++
@@ -1,2 +0,0 @@
-from model.ST_Encoder.ST_Encoder import ST_Enc
-from model.ST_Encoder.args import parse_args
--- model/__init__.py
+++
@@ -1,8 +0,0 @@
-from model.model_adapter import (
-    load_model,
-    get_conversation_template,
-    add_model_args,
-)
-
-from model.ST_Llama import STLlamaForCausalLM, load_model_pretrained
-
